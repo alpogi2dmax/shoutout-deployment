896b34610b813b94a2ee0e9d8f0156cc42d12e19
Al Cerdan Lico <alcerdan@yahoo.com>
2025-03-28 12:01:13 -0700

    add User

--- server/models.py
+++ server/models.py
@@ -4,3 +4,9 @@ from sqlalchemy.ext.associationproxy import association_proxy
 from server.config import db
 
 # Models go here!
+
+class User(db.Model):
+    __tablename__ = 'users'
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False, unique=True)
--- server/seed.py
+++ server/seed.py
@@ -8,10 +8,23 @@ from faker import Faker
 
 # Local imports
 from app import app
-from models import db
+from models import db, User
 
 if __name__ == '__main__':
     fake = Faker()
     with app.app_context():
         print("Starting seed...")
         # Seed code goes here!
+
+        print("Clearing db...")
+        User.query.delete()
+        db.session.commit()
+
+        print("Seeding users...")
+        users = []
+        user1 = User(
+            username='lightyagami'
+        )
+        users.append(user1)
+        db.session.add_all(users)
+        db.session.commit()
