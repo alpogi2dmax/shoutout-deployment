2d0163fa40d291769d4977da801864dff847421e
Al Cerdan Lico <alcerdan@yahoo.com>
2025-03-28 12:08:04 -0700

    moved Procfile

--- server/app.py
+++ server/app.py
@@ -7,7 +7,7 @@ from flask import request, send_from_directory
 from flask_restful import Resource
 
 # Local imports
-from server.config import app, db, api
+from config import app, db, api
 # Add your model imports
 from models import User
 
--- server/models.py
+++ server/models.py
@@ -1,7 +1,7 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from server.config import db
+from config import db
 
 # Models go here!
 
